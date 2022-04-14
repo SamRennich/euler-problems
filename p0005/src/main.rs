97df7ef66ba27c799958b1ef79daeee8a7910486
SamRennich <samuelrennich@gmail.com>
2022-04-14 17:38:35 -0400

    reformatted to stay within 80 chars

--- p0005/src/main.rs
+++ p0005/src/main.rs
@@ -3,7 +3,8 @@ Problem 5: Smallest Multiple
 
 2520 is the smallest number that can be divided by each of
 the numbers from 1 to 10 without any remainder.
-What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+What is the smallest positive number that is evenly divisible
+by all of the numbers from 1 to 20?
 
 Answer: 232792560
 */
@@ -28,13 +29,13 @@ fn main() {
         prime_factors.push(primes::factors(i as u64));
     }
 
-    let mut answer_factors = vec![];
+    let mut answer = vec![];
     for factor_list in prime_factors {
-        answer_factors.append(&mut remainder(factor_list, answer_factors.clone()));
+        answer.append(&mut remainder(factor_list, answer.clone()));
     }
 
     let mut product = 1;
-    for i in answer_factors {
+    for i in answer {
         product *= i;
     }
 
