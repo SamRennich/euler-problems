8c48935563043cd2b220dd4ed42233ac1aaac650
SamRennich <samuelrennich@gmail.com>
2022-04-14 13:12:28 -0400

    depracated old c++ and python files, will replace with rust over time

---
+++ p0007/src/main.rs
@@ -0,0 +1,21 @@
+/*
+Problem 7: 10001st Prime
+
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13,
+we can see that the 6th prime is 13.
+What is the 10001st prime number?
+
+Answer: 104743
+Solved: May 24, 2021
+*/
+
+// const RANGE: i32 = 10001;
+
+use primes::PrimeSet;
+
+fn main() {
+    let pset = PrimeSet::new();
+    // for (ix, n) in pset.iter().enumerate().skip(1_000).take(10) {
+    //     println!("Prime {}: {}", ix, n);
+    // }
+}
