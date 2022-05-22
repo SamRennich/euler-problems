6809dbd22936c9b484024e4e4b8c398ab0f83375
samrennich <samuelrennich@gmail.com>
2022-05-22 18:08:17 -0400

    changed range into limit (better word)

--- p0001/src/main.rs
+++ p0001/src/main.rs
@@ -9,12 +9,12 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 Answer: 233168
 */
 
-const RANGE: i32 = 999;
+const LIMIT: i32 = 999;
 
 fn main() {
-	let sum = |range| ((range as f64 / 2.0) * (range as f64 + 1.0)) as i32;
+	let sum = |limit| ((limit as f64 / 2.0) * (limit as f64 + 1.0)) as i32;
 
-	let total = sum(RANGE / 3) * 3 + sum(RANGE / 5) * 5 - sum(RANGE / 15) * 15;
+	let total = sum(LIMIT / 3) * 3 + sum(LIMIT / 5) * 5 - sum(LIMIT / 15) * 15;
 
 	println!("{}", total);
 }
