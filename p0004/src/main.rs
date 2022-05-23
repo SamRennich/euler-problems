265bd7ae5c5370e41f89e32b29a4c2a02ef5ecff
samrennich <samuelrennich@gmail.com>
2022-05-22 20:11:04 -0400

    changed word use

--- p0004/src/main.rs
+++ p0004/src/main.rs
@@ -10,7 +10,7 @@ Answer: 906609
 */
 
 const DIGITS: u32 = 3;
-const RANGE: i32 = i32::pow(10, DIGITS);
+const LIMIT: i32 = i32::pow(10, DIGITS);
 
 fn main() {
 	let rev = |num: i32| {
@@ -24,8 +24,8 @@ fn main() {
 
 	let mut largest_palidrome = 0;
 
-	for i in 1..RANGE {
-		for j in 1..RANGE {
+	for i in 1..LIMIT {
+		for j in 1..LIMIT {
 			let product = i * j;
 			if product > largest_palidrome && product == rev(product) {
 				largest_palidrome = product;
